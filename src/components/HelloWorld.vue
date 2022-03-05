a2be4237a16eeb2a30c883f24e848c44391c2e39
mohit-nigam77 <100343101+mohit-nigam77@users.noreply.github.com>
2022-03-05 13:44:08 +0530

    Set env file

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ siteName }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -35,6 +35,11 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      siteName: process.env.VUE_APP_TITLE
+    }
   }
 }
 </script>

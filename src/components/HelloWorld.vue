6cff52a1eebd1ad7002219a35c9dde9d7836c29a
mohit-nigam77 <100343101+mohit-nigam77@users.noreply.github.com>
2022-03-06 20:15:03 +0530

    Implemented bootstrap blog design

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,62 +1,61 @@
 <template>
   <div class="hello">
-    <h1>{{ siteName }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <HeaderComponent />
+    <main role="main">
+      <JumboTron />
+
+      <div class="album py-5 bg-light">
+        <div class="container">
+          <div class="row">
+            <CardComponent />
+          </div>
+        </div>
+      </div>
+    </main>
+
+    <FooterComponent />
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import HeaderComponent from './HeaderComponent.vue'
+import JumboTron from './Jumbotron.vue'
+import CardComponent from './CardComponent.vue'
+import FooterComponent from './FooterComponent.vue'
+import axios from "axios";
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
+    msg: String,
+  },
+  components:{
+    HeaderComponent,
+    JumboTron,
+    CardComponent,
+    FooterComponent
   },
   data() {
     return {
       siteName: process.env.VUE_APP_TITLE,
       data: [],
       results: [],
-    }
+    };
   },
   async mounted() {
     await axios
-      .get(`${process.env.VUE_APP_API_URL}characters?apikey=${process.env.VUE_APP_API_KEY}`)
-      .then(response => {
-        this.data = response.data.data
-        this.results = response.data.data.results
+      .get(
+        `${process.env.VUE_APP_API_URL}characters?apikey=${process.env.VUE_APP_API_KEY}`
+      )
+      .then((response) => {
+        this.data = response.data.data;
+        this.results = response.data.data.results;
       })
-      .catch(error => {
-        console.log(error)
-        this.errored = true
-      })
-  }
-}
+      .catch((error) => {
+        console.log(error);
+        this.errored = true;
+      });
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
